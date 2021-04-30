eebe507eb28b0385fd17b3e89a19683b9cc69c65
stph-phm <stephanie@legsm.com>
2021-04-30 13:18:54 +0200

    structurer bdd, ajouter les données, et faire les relations entre 2 tables

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,23 +2,55 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Liior\Faker\Prices;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
 
 class AppFixtures extends Fixture
 {
+    protected $slugger;
+
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
+
     public function load(ObjectManager $manager)
     {
-        for ($p = 1; $p < 100; $p++) {
-            $product = new Product;
-            $product
-                ->setName("Produit n°$p")
-                ->setPrice(mt_rand(100, 200))
-                ->setSlug("produit-n-$p");
+        $faker = Factory::create('fr_FR');
+        $faker->addProvider(new Prices($faker));
+        //$faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
+        $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
 
-            $manager->persist($product);
+        for ($i=0; $i < 3; $i++) { 
+            $category = new Category;
+            $category
+                ->setName($faker->sentence())
+                ->setSlug(strtolower($this->slugger->slug($category->getName())));
+            $manager->persist($category);
+     
+
+            for ($p = 1; $p < mt_rand(15, 20); $p++) {
+                $product = new Product;
+                $product
+                    //->setName($faker->productName())
+                    ->setName($faker->sentence())
+                    ->setPrice($faker->price(4000, 20000))
+                    ->setSlug(strtolower($this->slugger->slug($product->getName())))
+                    ->setCategory($category)
+                    ->setShortDescription($faker->paragraph())
+                    ->setPicture($faker->imageUrl(400, 400, true));
+    
+                $manager->persist($product);
+            }
         }
+
+       
 
         $manager->flush();
     }
