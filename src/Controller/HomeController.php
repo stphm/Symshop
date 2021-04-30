5b8f1f592f34bdb6753f8e896f0a9567bb990805
stph-phm <stephanie@legsm.com>
2021-04-30 13:28:21 +0200

    page home avec les données de la dbb

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,12 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function homepage(): Response
+    public function homepage(ProductRepository $productRepository): Response
     {
-        return $this->render('homepage.html.twig');
+       $products = $productRepository->findBy([], [], 3);
+
+        return $this->render('home.html.twig', [
+            'products' => $products
+        ]);
     }
 }
